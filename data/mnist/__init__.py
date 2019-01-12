d4ec2a83a207bdd68a9f81caca551bf052a1482f
Sebastian Brodehl <brodehl@uni-mainz.de>
2019-01-12 20:36:03 +0100

    Use keras.dataset method for MNIST data set.

--- data/mnist/__init__.py
+++ data/mnist/__init__.py
@@ -1,119 +1,37 @@
-#  Copyright 2018 The TensorFlow Authors. All Rights Reserved.
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-"""tf.data.Dataset interface to the MNIST dataset."""
-
-import gzip
-import os
-import shutil
-import tempfile
-
-import numpy as np
-from six.moves import urllib
-import tensorflow as tf
-
-import multiprocessing
 from template import BaseDataSampler
 
-
-def read32(bytestream):
-    """Read 4 bytes from bytestream as an unsigned 32-bit integer."""
-    dt = np.dtype(np.uint32).newbyteorder('>')
-    return np.frombuffer(bytestream.read(4), dtype=dt)[0]
-
-
-def check_image_file_header(filename):
-    """Validate that filename corresponds to images for the MNIST dataset."""
-    with tf.gfile.Open(filename, 'rb') as f:
-        magic = read32(f)
-    read32(f)    # num_images, unused
-    rows = read32(f)
-    cols = read32(f)
-    if magic != 2051:
-        raise ValueError('Invalid magic number %d in MNIST file %s' % (magic, f.name))
-    if rows != 28 or cols != 28:
-        raise ValueError('Invalid MNIST file %s: Expected 28x28 images, found %dx%d' % (f.name, rows, cols))
-
-
-def check_labels_file_header(filename):
-
-    """Validate that filename corresponds to labels for the MNIST dataset."""
-    with tf.gfile.Open(filename, 'rb') as f:
-        magic = read32(f)
-    read32(f)    # num_items, unused
-    if magic != 2049:
-        raise ValueError('Invalid magic number %d in MNIST file %s' % (magic, f.name))
-
-
-def download(directory, filename):
-    """Download (and unzip) a file from the MNIST dataset if not already done."""
-    filepath = os.path.join(directory, filename)
-    if tf.gfile.Exists(filepath):
-        return filepath
-    if not tf.gfile.Exists(directory):
-        tf.gfile.MakeDirs(directory)
-    # CVDF mirror of http://yann.lecun.com/exdb/mnist/
-    url = 'https://storage.googleapis.com/cvdf-datasets/mnist/' + filename + '.gz'
-    _, zipped_filepath = tempfile.mkstemp(suffix='.gz')
-    print('Downloading %s to %s' % (url, zipped_filepath))
-    urllib.request.urlretrieve(url, zipped_filepath)
-    with gzip.open(zipped_filepath, 'rb') as f_in, tf.gfile.Open(filepath, 'wb') as f_out:
-        shutil.copyfileobj(f_in, f_out)
-    os.remove(zipped_filepath)
-    return filepath
-
-
-def dataset(directory, images_file, labels_file):
-    """Download and parse MNIST dataset."""
-    images_file = download(directory, images_file)
-    labels_file = download(directory, labels_file)
-
-    check_image_file_header(images_file)
-    check_labels_file_header(labels_file)
-
-    def decode_image(image):
-        # Normalize from [0, 255] to [0.0, 1.0]
-        image = tf.decode_raw(image, tf.uint8)
-        image = tf.cast(image, tf.float32)
-        image = tf.reshape(image, [784])
-        return image / 255.0
-
-    def decode_label(label):
-        label = tf.decode_raw(label, tf.uint8)    # tf.string -> [tf.uint8]
-        label = tf.reshape(label, [])    # label is a scalar
-        return tf.to_int32(label)
-
-    images = tf.data.FixedLengthRecordDataset(
-            images_file, 28 * 28, header_bytes=16).map(
-            decode_image, num_parallel_calls=multiprocessing.cpu_count())
-    labels = tf.data.FixedLengthRecordDataset(
-            labels_file, 1, header_bytes=8).map(
-            decode_label, num_parallel_calls=multiprocessing.cpu_count())
-    return tf.data.Dataset.zip((images, labels))
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
 
 
 class DataSampler(BaseDataSampler):
 
     def __init__(self, data_dir):
-        self.data_dir = data_dir
+        super().__init__(data_dir)
+
+        mnist = keras.datasets.fashion_mnist
+        (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+        # normalize the values to [0,1] and of type float32
+        train_images = train_images.astype(np.float32)
+        test_images = test_images.astype(np.float32)
+        train_images = train_images / 255.0
+        test_images = test_images / 255.0
+        self.training_data = (train_images, train_labels.astype(np.int64))  # self.get_one_hot(train_labels, 10))
+        self.testing_data = (test_images, test_labels.astype(np.int64))  # self.get_one_hot(test_labels, 10))
+
+    @staticmethod
+    def get_one_hot(targets, nb_classes):
+        res = np.eye(nb_classes)[np.array(targets).reshape(-1)]
+        return (res.reshape(list(targets.shape) + [nb_classes])).astype(np.int32)
 
     def training(self):
-        """tf.data.Dataset object for MNIST training data."""
-        return dataset(str(self.data_dir), 'train-images-idx3-ubyte', 'train-labels-idx1-ubyte')
+        """tf.data.Dataset object for the MNIST training data."""
+        return tf.data.Dataset.from_tensor_slices(self.training_data)
 
     def testing(self):
-        """tf.data.Dataset object for MNIST test data."""
-        return dataset(str(self.data_dir), 't10k-images-idx3-ubyte', 't10k-labels-idx1-ubyte')
+        """tf.data.Dataset object for the MNIST test data."""
+        return tf.data.Dataset.from_tensor_slices(self.testing_data)
 
     def validation(self):
-        raise NotImplementedError
+        raise NotImplementedError("No validation data available for MNIST")
