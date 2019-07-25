c7e2c87a44160b92df0aa8b5399de68b2bba52dc
Sebastian Brodehl <brodehl@uni-mainz.de>
2019-07-25 13:06:06 +0200

    Convert to Keras layers.

--- model/rcnn/network.py
+++ model/rcnn/network.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import tensorflow.contrib.layers as tfl
 
 """Copied from the almighty Christian Hundt;
 
@@ -13,31 +12,35 @@ def prelu(net):
     return tf.maximum(alpha * net, net)
 
 
-def residual_conv_block(net, num_filters, kernel_size, stride, is_training=True):
+def residual_conv_block(net, num_filters, kernel_size, stride):
+    layers = tf.keras.layers
+
     # let us cache the input tensor and downsample it
-    inp = tfl.avg_pool2d(net, kernel_size, stride, padding="SAME")
+    inp = layers.AveragePooling2D(kernel_size, stride, padding="SAME")(net)
 
     # now convolve with stride (potential downsampling)
-    net = tfl.conv2d(net, num_filters, kernel_size, stride, activation_fn=tf.identity, padding="SAME")
+    net = layers.Conv2D(num_filters, kernel_size, stride, padding="SAME")(net)
 
     # normalize the output
-    net = tfl.batch_norm(net, is_training=is_training, activation_fn=tf.identity)
+    net = layers.BatchNormalization()(net)
 
     # now convolve again but do not downsample
-    net = tfl.conv2d(net, num_filters, kernel_size, stride=1, activation_fn=tf.identity, padding="SAME")
+    net = layers.Conv2D(num_filters, kernel_size, stride, strides=(1, 1), padding="SAME")(net)
 
-    return prelu(tf.concat((net, inp), axis=-1))
+    return prelu(layers.concatenate((net, inp), axis=-1))
 
 
-def network(X, Y, mode=tf.estimator.ModeKeys.TRAIN, classes=None):
-    net = tf.identity(X)
+def network(x, y, mode=tf.estimator.ModeKeys.TRAIN, classes=None):
+    layers = tf.keras.layers
+
+    net = tf.identity(x)
 
     net = residual_conv_block(net, 16, 3, 2)
     net = residual_conv_block(net, 32, 3, 2)
     net = residual_conv_block(net, 64, 3, 2)
     net = residual_conv_block(net, 128, 3, 2)
 
-    net = tf.reduce_mean(net, axis=(1, 2))
-    net = tfl.fully_connected(net, 10, activation_fn=tf.identity)
+    net = layers.GlobalAveragePooling2D()(net)
+    net = layers.Dense(classes)(net)
 
     return net
